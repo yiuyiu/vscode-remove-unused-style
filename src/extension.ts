3fc86a1903d5e7c30380efbba946264f1b408bab
xixi <yu.yi@ctrip.com>
2021-03-09 11:12:56 +0800

    docs

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import removeUnusedCss from './removeUnusedCss';
+import removeUnusedStyle from './removeUnusedStyle';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -9,7 +9,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerTextEditorCommand('removeUnusedCss.removeUnusedCss', removeUnusedCss);
+	let disposable = vscode.commands.registerTextEditorCommand('removeUnusedStyle', removeUnusedStyle);
 	context.subscriptions.push(disposable);
 }
 
